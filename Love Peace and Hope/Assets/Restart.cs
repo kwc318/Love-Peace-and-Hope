ce108b82711a6569afb16bcd7674943749fa363f
kwc318 <kwc318@nyu.edu>
2019-03-21 23:42:19 -0400

    Changes on 3/21/19

---
+++ Love Peace and Hope/Assets/Restart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Restart : MonoBehaviour
+{
+
+    public float timer;
+    public GameObject bg;
+    public Button start;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("start");
+            SceneManager.LoadScene(sceneName:"Main Menu");
+        }
+        
+        timer -= Time.deltaTime;
+        //Debug.Log(timer);
+        if (timer <= 0)
+        {
+            Destroy(bg);
+        }
+    }
+
+    public void ChangeScene()
+    {
+        Debug.Log("play");
+        SceneManager.LoadScene(sceneName:"Gameplay");
+    }
+}
---
+++ Love Peace and Hope/Assets/start.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void TaskOnClick()
+    {
+        Debug.Log("next");
+//        //SceneManager.LoadScene("Kimmy");
+//        fade.GetComponent<Animator>().Play("Fadeout");
+//        on = true;
+//        begin.Play();
+    }
+}
